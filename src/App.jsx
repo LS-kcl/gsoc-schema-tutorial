809e5839f5973a546f9c36c7f3f0af74f87779c2
Luke <lbmoogs@gmail.com>
2024-03-22 17:02:24 +0000

    fix: Correct styling to divide page into columns

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,25 @@ import "./styles.css"
 export default function App() {
   const [inputText, setInputText] = useState("")
 
-	return (
-	<>
-	<div className="tutorial-half">
-		<h1>Tutorial Half</h1>
-		<p>Some text here</p>
-	</div>
-	<div className="input-half">
-	<h1>Input Half</h1>
-	<form className="new-item-form">
-		<div className="form-row">
-			<input value={inputText} onChange={e => setInputText(e.target.value)} type="text" id="item"/>
+  function handleSubmit(e) {
+    e.preventDefault()
+  }
 
-		</div>
-	</form>
+	return (
+	<div className="page">
+    <div className="tutorial-half">
+      <h1>Tutorial Half</h1>
+      <h3>Rendered text from the input:</h3>
+      <p>{inputText}</p>
+    </div>
+    <div className="input-half">
+    <h1>Input Half</h1>
+    <form className="new-item-form">
+      <div className="form-row">
+        <input value={inputText} onChange={e => setInputText(e.target.value)} type="text" id="item"/>
+      </div>
+    </form>
+    </div>
 	</div>
-	</>
 	)
 }
